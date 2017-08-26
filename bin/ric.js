d6ea5d7aedeb66bf267d6ab5d25b8ecf4be1d7dd
M.K. Safi <msafi@msafi.com>
2017-08-25 22:14:38 -0700

    Nice working state

--- bin/ric.js
+++ bin/ric.js
@@ -20,102 +20,88 @@ const url_1 = require("url");
 const program = require("commander");
 // Import untyped JavaScript files
 const Github = require('github-api');
+// Initialize shell ouput coloring functions
 const name = chalk.italic.blue;
 const error = chalk.bold.underline.red;
 // Constants
 const pullRequestUrlRegex = /\/pull\/\d+/g;
 const pathnameParsingRegex = /^\/(.+?)\/(.+?)(?:\/|$)(?:pull\/(\d+))?/;
 const cwd = process.cwd();
+const { VIC_EDITOR_COMMAND = 'code --wait' } = process.env;
 // Application spinner
 const applicationSpinner = ora();
+// Setup arguments through the commander.js CLI framework
 program
     .usage('[options] <GitHub repo or pull-request URL>')
-    .option('-d, --deep [value]', 'Specify whether to clone repos deep or shallow. By default PRs are ' +
+    .option('-d, --deep [boolean]', 'Specify whether to clone repos deep or shallow. By default PRs are ' +
     'cloned with full depth and repos are cloned with depth level of 1.')
     .parse(process.argv);
+// If user doesn't provide any arguments, we'll display help
+if (!program.args.length) {
+    program.outputHelp();
+    shelljs.exit(1);
+}
+// Kick off the program
+main();
+// Main function
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        const [src] = program.args;
-        const { pathname } = new url_1.URL(src);
-        const isPr = getIsPr(pathname);
-        const { deep: isDeep = isPr ? true : false } = program;
-        const [, srcUser = '', srcRepoName = '', prNumber = '0'] = pathname.match(pathnameParsingRegex) || [];
-        const srcRepoFullName = `${srcUser}/${srcRepoName}`;
-        const userFriendlyName = (isPr) ? `${srcRepoFullName}#${prNumber}` : srcRepoFullName;
-        const dirName = path.join(os.tmpdir(), `ric-${srcRepoName}-${Date.now()}`);
-        applicationSpinner.start('Creating temporary directory');
-        fs.mkdirSync(dirName);
-        applicationSpinner.succeed(`Files will temporarily reside in ${name(dirName)}`);
-        let repoUrl = `https://github.com/${srcRepoFullName}`;
-        let branch = 'master';
-        if (isPr) {
-            const gh = new Github();
-            const repo = gh.getRepo(srcUser, srcRepoName);
-            applicationSpinner.start(`Retrieving info for PR ${name(userFriendlyName)}`);
-            const pr = yield repo.getPullRequest(prNumber);
-            const headRepo = _.get(pr, 'data.head', {});
-            const headRepoName = _.get(headRepo, 'repo.name', '');
-            const [user, branch] = _.get(headRepo, 'label', '').split(':');
-            repoUrl = `https://github.com/${user}/${headRepoName}`;
+        try {
+            const [src] = program.args;
+            const { pathname } = new url_1.URL(src);
+            const isPr = checkIsPr(pathname);
+            const { deep: isDeep = isPr ? true : false } = program;
+            const [, srcUser = '', srcRepoName = '', prNumber = '0'] = pathname.match(pathnameParsingRegex) || [];
+            const srcRepoFullName = `${srcUser}/${srcRepoName}`;
+            const userFriendlyName = (isPr) ? `${srcRepoFullName}#${prNumber}` : srcRepoFullName;
+            const dirName = path.join(os.tmpdir(), `ric-${srcRepoName}-${Date.now()}`);
+            fs.mkdirSync(dirName);
+            applicationSpinner.succeed(`Files will temporarily reside in ${name(dirName)}`);
+            let repoUrl = `https://github.com/${srcRepoFullName}`;
+            let branch = 'master';
+            if (isPr) {
+                const gh = new Github();
+                const repo = gh.getRepo(srcUser, srcRepoName);
+                applicationSpinner.start(`Retrieving PR info ${name(userFriendlyName)}`);
+                const pr = yield repo.getPullRequest(prNumber);
+                const prRepo = _.get(pr, 'data.head', {});
+                const prRepoName = _.get(prRepo, 'repo.name', '');
+                const [prUser, prBranch] = _.get(prRepo, 'label', '').split(':');
+                branch = prBranch;
+                repoUrl = `https://github.com/${prUser}/${prRepoName}`;
+            }
+            shelljs.cd(dirName);
+            applicationSpinner.start(`Cloning ${name(`${repoUrl}`)} into temporary directory`);
+            yield exec(`GIT_TERMINAL_PROMPT=0 git clone ${(isDeep) ? '' : '--depth 1 '}${repoUrl} .`);
+            applicationSpinner.start(`Checking out ${name(branch)}`);
+            yield exec(`git checkout ${branch}`);
+            shelljs.cd(cwd);
+            applicationSpinner.stop();
+            const reviewingRepoSpinner = ora({
+                text: `Reviewing ${name(userFriendlyName)} in editor`,
+                spinner: { frames: ['📖 '] }
+            }).start();
+            yield exec(`${VIC_EDITOR_COMMAND} ${dirName}`);
+            reviewingRepoSpinner.succeed(`Done reviewing ${name(userFriendlyName)}`);
         }
-        shelljs.cd(dirName);
-        applicationSpinner.start(`Cloning ${name(`${repoUrl}`)} into the temporary directory}`);
-        yield exec(`git clone ${(isDeep) ? '' : '--depth 1 '}${repoUrl} .`);
-        applicationSpinner.start(`Checking out ${name(branch)}`);
-        yield exec(`git checkout ${branch}`);
-        shelljs.cd(cwd);
-        applicationSpinner.stop();
-        const reviewingRepoSpinner = ora({
-            text: `Reviewing ${name(userFriendlyName)} in ${name('VS Code')}`,
-            spinner: { frames: ['📖 '] }
-        }).start();
-        yield exec(`code ${dirName} --wait`);
-        reviewingRepoSpinner.succeed(`Done reviewing ${name(userFriendlyName)}`);
+        catch (err) {
+            console.error(error('\n\nAn error occurred:\n'));
+            console.log(err.stack ? err.stack : err);
+            shelljs.exit(1);
+        }
     });
 }
-// // async function reviewPullRequest(
-// //   srcUser: string, 
-// //   srcRepoName: string,
-// //   prNumber: string,
-// //   tmpdirName: string,
-// //   srcUserFriendlyName: string
-// // ) {
-// //   const cwd = process.cwd()
-// //   const gh = new Github()
-// //   const repo = gh.getRepo(srcUser, srcRepoName)
-// //   applicationSpinner.start(`Retrieving info for PR ${name(srcUserFriendlyName)}`)
-// //   const pr = await repo.getPullRequest(prNumber)
-// //   const headRepo = _.get(pr, 'data.head', {})
-// //   const headRepoName = _.get(headRepo, 'repo.name', '')
-// //   const [user, branch] = _.get(headRepo, 'label', '').split(':')
-// //   shelljs.cd(tmpdirName)
-// //   applicationSpinner.start(`Cloning ${name(`${user}/${headRepoName}`)} into ${name(tmpdirName)}`)
-// //   await exec(`git clone https://github.com/${user}/${headRepoName} .`)
-// //   applicationSpinner.start(`Checking out ${name(branch)}`)
-// //   await exec(`git checkout ${branch}`)
-// //   shelljs.cd(cwd)
-// // }
-// // async function reviewRepo(src: string, tmpdirName: string) {
-// //   const repo = parse(src)
-// //   const hash = await getHash(repo, {})
-// //   const fileName = `${hash}.tar.gz`
-// //   const file = `${tmpdirName}/${fileName}`
-// //   const url = `${repo.url}/archive/${fileName}`
-// //   await fetch(url, file)
-// //   await untar(file, tmpdirName)
-// // }
 function exec(str) {
-    return new Promise((resolve) => shelljs.exec(str, { silent: true }, resolve));
-    // return new Promise((resolve, reject) => shelljs.exec(str, {silent: true}, (code: number, _1, errorMsg) => {
-    //   if (code === 0) {
-    //     resolve()
-    //   } else {
-    //     console.error(error('\nAn error occurred:\n'))
-    //     console.error(errorMsg)
-    //     shelljs.exit(1)
-    //   }
-    // }))
+    return new Promise((resolve, reject) => shelljs.exec(str, { silent: true }, (code, _1, errorMsg) => {
+        if (code === 0) {
+            resolve();
+        }
+        else {
+            console.log(errorMsg);
+            reject(errorMsg);
+        }
+    }));
 }
-function getIsPr(pathname) {
+function checkIsPr(pathname) {
     return pullRequestUrlRegex.test(pathname);
 }
